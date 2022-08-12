1e28ae69fb90b94913f815e68896ff9ce4197422
Jumail Mundekkat <mundekkat@hotmail.com>
2022-08-13 02:05:07 +1000

    Add login start route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import Express, { NextFunction, Request, Response } from 'express';
-import { AttestationResult, Fido2Lib } from 'fido2-lib';
+import { AssertionResult, AttestationResult, Fido2Lib } from 'fido2-lib';
 import { generateKeyPair, GenerateKeyPairResult, JWTPayload, jwtVerify, SignJWT } from 'jose';
 
 /** Server runtime port */
@@ -43,15 +43,22 @@ type ChallengeJWT = JWTPayload & {
   challenge: string; 
 };
 
+/** Request body for /login/start */
 interface LoginStartBody {
   email?: string;
+}
+
+/** Request body for /login/finish */
+interface LoginFinishBody {
+  token: string;
+  result: AssertionResult;
 }
 
 /** Data to represent a FIDO2 credential */
 interface Credential {
   counter: number;
   credentialId: ArrayBuffer;
-  publicKey: ArrayBuffer;
+  publicKey: string;
 }
 
 /** Basic user data to persist something resembling a user */
@@ -185,8 +192,9 @@ app.post('/register/finish', runAsync(async (req, res): Promise<void> => {
       publicKey: attestationRes.authnrData.get('credentialPublicKeyPem')
     });
 
+    // Return success
     res.json({ 'status': 'ok' });
-
+    return;
   } catch (e) {
     console.error(e);
     res.status(403).json({ 'error': 'Attestation failed' });
@@ -198,7 +206,7 @@ app.post('/register/finish', runAsync(async (req, res): Promise<void> => {
  * Login start route, provided a LoginStartBody 
  * Returns 200 with a login challenge on sucess, 403 on invalid username, 400 on bad request 
 */
-app.post('/login', runAsync(async (req, res): Promise<void> => {
+app.post('/login/start', runAsync(async (req, res): Promise<void> => {
   // Default the body to an empty object
   const rawBody = req.body != null ? req.body : "{}";
   // Parse and validate request body
@@ -251,6 +259,83 @@ app.post('/login', runAsync(async (req, res): Promise<void> => {
     'options': opts
   });
   return;
+}));
+
+/** 
+ * Login start route, provided a LoginFinishBody 
+ * Returns 200 with a login challenge on sucess, 403 on authentication failure, 400 on bad request 
+*/
+app.use('/login/finish', runAsync(async (req, res): Promise<void> => {
+  // Parse and validate request body
+  let body: LoginFinishBody = null;
+  try {
+    body = JSON.parse(req.body);
+    assert(typeof body.token === 'string');
+    assert(body.result !== null || body.result !== undefined);
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({ 'error': 'Invalid request' });
+    return;
+  }
+
+  // Verify and decode the JWT
+  const jwtDecode = await jwtVerify(body.token, serverKp.publicKey);
+  const jwt = <ChallengeJWT> jwtDecode.payload;
+
+  try {
+    // Find the credentials that the challenge was signed by
+    let user: UserData = null;
+    let cred: Credential = null;
+    if (jwt.userName != null) {
+      // If a username is present, look for the credentials under that user
+      user = users.get(jwt.userName);
+      cred = user.credentials.filter(c => c.credentialId == body.result.rawId)[0];
+    } else {
+      // If no username is present, look through all the users for a credential that matches
+      for (const curUser of users.values()) {
+        const potentialCreds = curUser.credentials.filter(c => c.credentialId == body.result.rawId);
+        if (potentialCreds.length > 0) {
+          // Keep track of the user and matching credential
+          user = curUser;
+          cred = potentialCreds[0];
+          break;
+        }
+      }
+    }
+
+    // If we couldn't find a matching set of credentials, throw a 403
+    if (cred == null) {
+      res.status(403).json({ 'error': 'Unknown credentials' });
+      return;
+    }
+
+    // Validate the assertion against the challenge
+    const assertionRes = await fido2.assertionResult(body.result, {
+      challenge: jwt.challenge,
+      origin: ORIGIN,
+      factor: 'first',
+      publicKey: cred.publicKey,
+      prevCounter: cred.counter,
+      userHandle: String.fromCharCode(...Array.from<number>(new Uint8Array(cred.credentialId)))
+    });
+
+    // Update the counter on the credential
+    cred.counter = assertionRes.authnrData.get("counter");
+
+    // Return success, along with the user object
+    res.json({ 
+      'status': 'ok', 
+      'user': {
+        'userName': user.userName,
+        'displayName': user.displayName
+      } 
+    });
+    return;
+  } catch (e) {
+    console.error(e);
+    res.status(403).json({ 'error': 'Assertion failed' });
+    return;
+  }
 }));
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`) });
