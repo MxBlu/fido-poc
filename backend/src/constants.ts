173c1f57c69356645a558b30b417dd3b1786117f
Jumail Mundekkat <mundekkat@hotmail.com>
2022-08-14 11:35:43 +1000

    Fix constants definitions

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -13,26 +13,27 @@ export const RP_NAME = "MxBlue Server";
  *
  * * Platform authenticators ('platform') - Uses a security feature built on top of a platform, i.e. Windows Hello, Face ID
  * * Roaming authenticators ('cross-platform') - Uses a physical security key, i.e. anything FIDO2 compliant like a Yubikey
+ *
+ * Setting this to undefined means either kind of authenticator is accepted
 */
-export const FIDO2_ATTACHMENT_REQUIREMENT: Attachment = null;
+export const FIDO2_ATTACHMENT_REQUIREMENT: Attachment = undefined;
 /** 
  * Setting this will instruct the platform whether or not to allow resident key login.
  * 
  * A resident key is a reference to a credential that is tied to the RP ID and stored by the authenticator.
  * 
- * * Setting this to null means its up to the platform to decide if it wants to store a resident key
- * * Setting this to false will discourage storing a resident key, and prevent resident key login
+ * * Setting this to false means its up to the platform to decide if it wants to store a resident key
  * * Setting this to true will require the platform to store a resident key
  */
-export const FIDO2_REQUIRE_RESIDENT_KEY: boolean | null = null;
+export const FIDO2_REQUIRE_RESIDENT_KEY = false;
 /**
  * Setting this will determine whether to require user verification
  * 
  * User verification is a validation that the platform has verified the users identity with the authenticator
  * 
- * * Setting this to null will let the platform decide whether to verify the user or not
+ * * Setting this to undefined will let the platform decide whether to verify the user or not
  * * Setting this to preferred will request the platform to verify if possible
  * * Setting this to discouraged will request the platform to not verify if possible
  * * Setting this to required will require the platform to verify
  */
-export const FIDO2_USER_VERIFICATION_REQUIREMENT: UserVerification = null;
+export const FIDO2_USER_VERIFICATION_REQUIREMENT: UserVerification = undefined;
--- frontend/src/request_interfaces.ts
+++ frontend/src/request_interfaces.ts
@@ -1,3 +1,8 @@
+/**
+ * Interfaces and models copied across from the backend code
+ * 
+ * All response interfaces have been modified to assume a successful response (e.g. LoginStartResponse.token is not null)
+ */
 
 /** Request body for /login/start */
 export interface LoginStartRequest {
@@ -52,7 +57,11 @@ export interface RegistrationFinishResponse {
   status: string;
 }
 
-/** Slight variation of the attestation request interface to facilitate transport over JSON */
+/** 
+ * Slight variation of the attestation request interface to facilitate transport over JSON 
+ * 
+ * Namely, all ArrayBuffers have been converted to base64 strings for transport
+*/
 export interface AttestationOptionsWireFormat {
   rp: { name: string; id: string; icon?: string };
   user: { id: string, name: string, displayName: string };
@@ -65,7 +74,11 @@ export interface AttestationOptionsWireFormat {
   extensions?: AuthenticationExtensionsClientInputs;
 }
 
-/** Slight variation of the attestation result interface to facilitate transport over JSON */
+/** 
+ * Slight variation of the attestation result interface to facilitate transport over JSON 
+ * 
+ * Namely, all ArrayBuffers have been converted to base64 strings for transport
+*/
 export interface AttestationResultWireFormat {
   id?: string;
   rawId?: string;
@@ -73,7 +86,11 @@ export interface AttestationResultWireFormat {
   response: { clientDataJSON: string; attestationObject: string };
 }
 
-/** Slight variation of the assertion request interface to facilitate transport over JSON */
+/** 
+ * Slight variation of the assertion request interface to facilitate transport over JSON 
+ * 
+ * Namely, all ArrayBuffers have been converted to base64 strings for transport
+*/
 export interface AssertionOptionsWireFormat {
   challenge: string;
   timeout?: number;
@@ -89,7 +106,11 @@ export interface AssertionOptionsWireFormat {
   }[];
 }
 
-/** Slight variation of the assertion result interface to facilitate transport over JSON */
+/** 
+ * Slight variation of the assertion result interface to facilitate transport over JSON 
+ * 
+ * Namely, all ArrayBuffers have been converted to base64 strings for transport
+*/
 export interface AssertionResultWireFormat {
   id?: string;
   rawId?: string;
